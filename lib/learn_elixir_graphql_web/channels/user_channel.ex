acdb1aade399718e12e8b4523fadcfda12dc49f5
Alan Vardy <alanvardy@gmail.com>
2020-02-04 10:13:06 -0800

    remove unneeded channel config

--- lib/learn_elixir_graphql_web/channels/user_channel.ex
+++ lib/learn_elixir_graphql_web/channels/user_channel.ex
@@ -4,16 +4,4 @@ defmodule LearnElixirGraphqlWeb.UserChannel do
   def join("users", _payload, socket) do
     {:ok, socket}
   end
-
-  def handle_in("create_user", %{"id" => id}, socket) do
-    broadcast(socket, "create_user", %{"id" => id})
-
-    {:noreply, socket}
-  end
-
-  def handle_in("update_user_preferences", %{"id" => id}, socket) do
-    broadcast(socket, "update_user_preferences", %{"id" => id})
-
-    {:noreply, socket}
-  end
 end
