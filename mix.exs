0298eb3dfb27429ef8a4a14c178fa18d040b19c5
Alan Vardy <alanvardy@gmail.com>
2020-02-26 13:47:27 -0800

    Set elixir version to 1.10.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LearnElixirGraphql.MixProject do
     [
       app: :learn_elixir_graphql,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
