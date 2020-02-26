61207a83ebd9ec9158e768cae130320caf5eb9fb
Alan Vardy <alanvardy@gmail.com>
2020-02-26 13:48:58 -0800

    Use agent instead of genserver

--- lib/learn_elixir_graphql/metrics/hit_tracker.ex
+++ lib/learn_elixir_graphql/metrics/hit_tracker.ex
@@ -1,39 +1,19 @@
 defmodule LearnElixirGraphql.Metrics.HitTracker do
   @moduledoc "Process which keeps track of the number of times any particular resolver has been hit"
-  use GenServer
-
-  # Client
+  use Agent
 
   @spec start_link(maybe_improper_list) :: :ignore | {:error, any} | {:ok, pid}
-  def start_link(default) when is_list(default) do
-    GenServer.start_link(__MODULE__, default, name: __MODULE__)
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
   @spec increment(binary) :: :ok
   def increment(key) do
-    GenServer.cast(__MODULE__, {:increment, key})
+    Agent.update(__MODULE__, fn state -> Map.update(state, key, 1, &(&1 + 1)) end)
   end
 
   @spec get(binary) :: non_neg_integer
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
-  end
-
-  # Server
-
-  @impl true
-  @spec init(any) :: {:ok, %{}}
-  def init(_) do
-    {:ok, %{}}
-  end
-
-  @impl true
-  def handle_cast({:increment, key}, state) do
-    {:noreply, Map.update(state, key, 1, &(&1 + 1))}
-  end
-
-  @impl true
-  def handle_call({:get, key}, _from, state) do
-    {:reply, Map.get(state, key, 0), state}
+    Agent.get(__MODULE__, fn state -> Map.get(state, key, 0) end)
   end
 end
